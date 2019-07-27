4a7f5a41b56ff6b778ac8339cefef0584a4be2b9
Adrien MILLE <adrien.mille.aer@gmail.com>
2019-07-28 01:20:01 +0200

    feat(ssr): add server side rendering support
    BREAKING CHANGE: replace the default export by nammed exports

---
+++ src/renderWithData.js
@@ -0,0 +1,29 @@
+import { createElement } from 'react';
+import SSRContext from './SSRContext';
+import SSRRenderer from './SSRRenderer';
+
+const renderWithData = (tree, render) => {
+  const renderer = new SSRRenderer();
+
+  const process = () => {
+    // wrap the tree with our provider to dispatch our SSR context
+    const element = createElement(
+      SSRContext.Provider,
+      { value: { ssr: true, renderer } },
+      tree,
+    );
+
+    // call the rendering method
+    const output = render(element);
+
+    // loop until there's no more promises
+    return renderer.hasPromises()
+      ? renderer.consume().then(process)
+      : [output, renderer.state];
+  };
+
+  // start the loop
+  return process();
+};
+
+export default renderWithData;
